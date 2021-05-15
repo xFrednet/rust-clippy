109a56333413b8e347726fc0a37e1bdf9c7675db
xFrednet <xFrednet@gmail.com>
2021-05-15 18:27:43 +0200

    Run boy run

--- clippy_lints/src/utils/internal_lints/metadata_collector.rs
+++ clippy_lints/src/utils/internal_lints/metadata_collector.rs
@@ -150,7 +150,7 @@ impl MetadataCollector {
         self.config
             .iter()
             .filter(|config| config.lints.iter().any(|lint| lint == lint_name))
-            .map(|config| config.to_string())
+            .map(str::to_string)
             .reduce(|acc, x| acc + &x)
             .map(|configurations| format!(CONFIGURATION_SECTION_TEMPLATE!(), configurations = configurations))
     }
