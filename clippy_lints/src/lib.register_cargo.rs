3841b3d02cb63e7396acc8e988a183c38dfb04d6
xFrednet <xFrednet@gmail.com>
2022-02-13 19:40:24 +0100

    Run `cargo dev update_lints`

--- clippy_lints/src/lib.register_cargo.rs
+++ clippy_lints/src/lib.register_cargo.rs
@@ -2,10 +2,10 @@
 // Use that command to update this file and do not edit by hand.
 // Manual edits will be overwritten.
 
-store.register_group(true, "clippy::cargo", Some("clippy_cargo"), vec![
-    LintId::of(cargo_common_metadata::CARGO_COMMON_METADATA),
-    LintId::of(feature_name::NEGATIVE_FEATURE_NAMES),
-    LintId::of(feature_name::REDUNDANT_FEATURE_NAMES),
-    LintId::of(multiple_crate_versions::MULTIPLE_CRATE_VERSIONS),
-    LintId::of(wildcard_dependencies::WILDCARD_DEPENDENCIES),
-])
+store.register_group(true, "clippy::cargo", Some("clippy_cargo"), [
+    Some(LintId::of(cargo_common_metadata::CARGO_COMMON_METADATA)),
+    Some(LintId::of(feature_name::NEGATIVE_FEATURE_NAMES)),
+    Some(LintId::of(feature_name::REDUNDANT_FEATURE_NAMES)),
+    Some(LintId::of(multiple_crate_versions::MULTIPLE_CRATE_VERSIONS)),
+    Some(LintId::of(wildcard_dependencies::WILDCARD_DEPENDENCIES)),
+].iter().copied().flatten().collect::<Vec<_>>())
